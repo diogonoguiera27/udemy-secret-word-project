621c0b908e5d43a6fc22b8fc20ee41f570fff1bd
diogonoguiera27 <diogoalvesnogueira3@gmail.com>
2025-06-20 14:18:06 -0300

    Alternando os estagios do jogo

--- src/App.js
+++ src/App.js
@@ -23,12 +23,21 @@ function App() {
   const [gameStage, setGamesStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
-  console.log(words)
+  // starts the secret word game
+  const startGame = () =>{
+    setGamesStage(stages[1].name)
+  }
+  const verifyLetter = () => {
+    setGamesStage(stages[2].name)
+  }
+  const retry= () => {
+    setGamesStage(stages[0].name)
+  }
   return (
     <div className="App">
-      {gameStage === "start" && <StartScreen/>} 
-      {gameStage === "game" && <Game/>} 
-      {gameStage === "end" && <GameOver/>} 
+      {gameStage === "start" && <StartScreen startGame={startGame}/>} 
+      {gameStage === "game" && <Game verifyLetter={verifyLetter}/>} 
+      {gameStage === "end" && <GameOver retry={retry}/>} 
     </div>
   );
 }
